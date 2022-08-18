4a0a4a485be3b9af611c2dd037b39f38fb465615
Fernando Tirelli <fernandotirelli@Fernandos-MacBook-Pro.local>
2022-08-17 23:10:37 -0300

    startup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import jwt_decode from 'jwt-decode';
+import { useEffect } from 'react';
+import { useGoogleOneTapLogin } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 import './App.css';
 
 function App() {
+  // const handleCallbackResponse = (response) => {
+  //   console.log({ credentials: response.credential });
+  //   const jwtDecoded = jwt_decode(response.credential);
+  //   console.log(jwtDecoded);
+  // };
+
+  // useEffect(() => {
+  //   /* global google */
+  //   google.accounts.id.initialize({
+  //     client_id:
+  //       '317200523271-mrrvvj4ubhvtljv19vt36o07s2e5qft8.apps.googleusercontent.com',
+  //     callback: handleCallbackResponse,
+  //   });
+
+  //   google?.accounts?.id.renderButton(document.getElementById('signInDiv'), {
+  //     theme: 'outline',
+  //     size: 'large',
+  //   });
+  // }, []);
+
+  // useGoogleOneTapLogin({
+  //   onSuccess: credentialResponse => {
+  //       console.log(credentialResponse);
+  //   },
+  //   onError: () => {
+  //     console.log('Login Failed')
+  //   }
+  // });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+        useOneTap
+        />
     </div>
   );
 }
